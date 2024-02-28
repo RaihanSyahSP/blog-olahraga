2ad60f3b55597834c007e01aaa86ba14e48252bb
Raihan Syah <raihansyah45@gmail.com>
2024-02-28 16:22:09 +0700

    feat:add get post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
@@ -13,11 +14,25 @@ class Post extends Model
 
     protected $fillable = ["title", "slug", "thumbnail", "body", "active", "youtube_link", "published_at", "user_id"];
 
+    protected $casts = [
+        'published_at' => 'datetime'
+    ];
+
     public function user(): BelongsTo{
         return $this->belongsTo(User::class);
     }
 
     public function categories(): BelongsToMany{
         return $this->belongsToMany(Category::class);
+    }
+
+    public function getFormattedDate()
+    {
+        return $this->published_at->format('d F Y');
+    }
+
+    public function shortBody($words = 30): string
+    {
+        return Str::words(strip_tags($this->body), $words);
     }
 }
