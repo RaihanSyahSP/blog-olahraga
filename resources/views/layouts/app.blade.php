8f7e14ddce6ad2495ea22aadbceff818c395da67
Raihan Syah <raihansyah45@gmail.com>
2024-03-01 16:35:08 +0700

    feat:add about page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Olahraga</title>
-    <meta name="author" content="">
-    <meta name="description" content="">
+    <meta name="author" content="blog olahraga">
+    <meta name="description" content="blog olahraga">
 
     <link href="{{URL::asset('css/filament/filament/app.css')}}" rel="stylesheet">
     
@@ -27,7 +27,7 @@
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
 </head>
-<body class="bg-white font-family-karla">
+<body class="bg-gray-50 bg-white font-family-karla">
 
     <!-- Top Bar Nav -->
     <nav class="w-full py-4 bg-blue-800 shadow">
