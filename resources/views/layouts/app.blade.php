5828404489db7f8856b4e3190bedc2dc0bfa4835
Raihan Syah <raihansyah45@gmail.com>
2024-03-04 23:50:13 +0700

    style:change navbar and color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'Blog Olahraga' }}</title>
+    <title>{{ $title ?? 'Sino Home Exercise' }}</title>
     <meta name="author" content="@yield('metaAuthor')">
     <meta name="description" content="@yield('metaDescription')">
 
     <link href="{{URL::asset('css/filament/filament/app.css')}}" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="{{URL::asset('images/logo.png')}}">
     
     <!-- Tailwind -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
@@ -31,62 +32,43 @@
     <script src="{{URL::asset('lightbox2/dist/js/lightbox-plus-jquery.min.js')}}"></script>
 </head>
 <body class="bg-gray-50 bg-white font-family-karla">
-
-    <!-- Top Bar Nav -->
-    <nav class="w-full py-4 bg-blue-800 shadow">
-        <div class="w-full container mx-auto flex flex-wrap items-center justify-end">
-
-            <div class="flex items-center text-lg no-underline text-white pr-6">
-                <a class="" href="#">
-                    <i class="fab fa-facebook"></i>
-                </a>
-                <a class="pl-6" href="#">
-                    <i class="fab fa-instagram"></i>
-                </a>
-                <a class="pl-6" href="#">
-                    <i class="fab fa-twitter"></i>
-                </a>
-                <a class="pl-6" href="#">
-                    <i class="fab fa-linkedin"></i>
+    
+        <!-- Topic Nav -->
+        <nav class="w-full py-4 bg-white  shadow  text-green-400 flex items-center flex-col sticky top-0 z-10 backdrop-filter backdrop-blur-lg bg-opacity-30 lg:justify-between lg:flex-row" x-data="{ open: false }">
+            <div class="hidden lg:block">
+                <img src="{{URL::asset('images/logo.png')}}" alt="logo" class="w-24" />
+            </div>
+            <div class="block sm:hidden">
+                <a
+                    href="#"
+                    class="block md:hidden text-base font-bold uppercase text-center flex justify-center items-center"
+                    @click="open = !open"
+                >
+                    Topics <i :class="open ? 'fa-chevron-down': 'fa-chevron-up'" class="fas ml-2"></i>
                 </a>
             </div>
-        </div>
-
-    </nav>
+            <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
+                <div class="w-full container ml-auto flex flex-col sm:flex-row  items-center justify-end text-sm font-semibold uppercase mt-0 px-6 py-2">
+                    <a href="{{route('home')}}" class=" px-2 text-center hover:bg-green-400 hover:text-white  rounded py-2 ">Home</a>
+                    @foreach($categories as $category)
+                        <a href="{{route('by-category', $category)}}" class="px-2 text-center hover:bg-green-400 hover:text-white  rounded py-2   ">{{$category->title}}</a>    
+                    @endforeach
+                    <a href="{{ route('about-us') }}" class="px-2 text-center hover:bg-green-400 hover:text-white  rounded py-2  ">About us</a>
+                </div>
+            </div>
+        </nav>
 
     <!-- Text Header -->
     <header class="w-full container mx-auto">
         <div class="flex flex-col items-center py-12">
-            <a class="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl" href="/">
-                Blog Olahraga
+            <a class="text-center font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl" href="/">
+                Sino Home Exercise
             </a>
             <p class="text-lg text-gray-600">
                 {{ \App\Models\TextWidget::getTitle('header') }}
             </p>
         </div>
     </header>
-
-    <!-- Topic Nav -->
-    <nav class="w-full py-4 border-t border-b bg-gray-100" x-data="{ open: false }">
-        <div class="block sm:hidden">
-            <a
-                href="#"
-                class="block md:hidden text-base font-bold uppercase text-center flex justify-center items-center"
-                @click="open = !open"
-            >
-                Topics <i :class="open ? 'fa-chevron-down': 'fa-chevron-up'" class="fas ml-2"></i>
-            </a>
-        </div>
-        <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
-            <div class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
-                <a href="{{route('home')}}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">Home</a>
-                @foreach($categories as $category)
-                    <a href="{{route('by-category', $category)}}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">{{$category->title}}</a>    
-                @endforeach
-                <a href="{{ route('about-us') }}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">About us</a>
-            </div>
-        </div>
-    </nav>
 
 
     <div class="container mx-auto flex flex-wrap py-6">
@@ -95,7 +77,7 @@
 
     </div>
 
-    <footer class="w-full border-t bg-white pb-12">
+    <footer class="w-full border-t bg-green-500 text-white">
         <div class="w-full container mx-auto flex flex-col items-center">
             <div class="flex flex-col md:flex-row text-center md:text-left md:justify-between py-6">
                 <a href="#" class="uppercase px-3">About Us</a>
@@ -103,7 +85,7 @@
                 <a href="#" class="uppercase px-3">Terms & Conditions</a>
                 <a href="#" class="uppercase px-3">Contact Us</a>
             </div>
-            <div class="uppercase pb-6">&copy; myblog.com</div>
+            <div class="uppercase pb-6">&copy; Sino Home Exercise</div>
         </div>
     </footer>
 
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -11,7 +11,7 @@
         <div class="w-full flex pt-6">
             @if ($prev)
                 <a href="{{ route('post', $prev) }}" class="w-1/2 bg-white shadow hover:shadow-md text-left p-6 flex-1">
-                    <p class="text-lg text-blue-800 font-bold flex items-center">
+                    <p class="text-lg text-green-800 font-bold flex items-center">
                         <i class="fas fa-arrow-left pr-1"></i> Previous
                     </p>
                     <p class="pt-2">{{ $prev->title }}</p>
@@ -20,7 +20,7 @@
 
             @if ($next)
                 <a href="{{ route('post', $next) }}" class="w-1/2 bg-white shadow hover:shadow-md text-right p-6 flex-1">
-                    <p class="text-lg text-blue-800 font-bold flex items-center justify-end">
+                    <p class="text-lg text-green-800 font-bold flex items-center justify-end">
                         Next <i class="fas fa-arrow-right pl-1"></i>
                     </p>
                     <p class="pt-2">{{ $next->title }}</p>
@@ -28,34 +28,6 @@
             @endif
         </div>
 
-        <div
-            class="w-full flex flex-col space-x-4 text-center md:text-left md:flex-row shadow bg-white mt-10 mb-10 p-6">
-            <div class="w-full md:w-1/5 flex justify-center md:justify-start pb-4">
-                <img src="https://source.unsplash.com/collection/1346951/150x150?sig=1"
-                    class="rounded-full shadow h-32 w-32" />
-            </div>
-            <div class="flex-1 flex flex-col justify-center md:justify-start">
-                <p class="font-semibold text-2xl">David</p>
-                <p class="pt-2">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Curabitur vel neque non libero suscipit suscipit eu eu urna.
-                </p>
-                <div class="flex items-center justify-center md:justify-start text-2xl no-underline text-blue-800 pt-4">
-                    <a class="" href="#">
-                        <i class="fab fa-facebook"></i>
-                    </a>
-                    <a class="pl-4" href="#">
-                        <i class="fab fa-instagram"></i>
-                    </a>
-                    <a class="pl-4" href="#">
-                        <i class="fab fa-twitter"></i>
-                    </a>
-                    <a class="pl-4" href="#">
-                        <i class="fab fa-linkedin"></i>
-                    </a>
-                </div>
-            </div>
-        </div>
     </section>
 
     <x-sidebar />
