78a8b3d8d2559bebb780a87136b6920c48b28515
Raihan Syah <raihansyah45@gmail.com>
2024-03-26 10:04:55 +0700

    feat:add sosial media contact

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -13,6 +13,31 @@
                 <div>
                     {!! $widget->content !!}
                 </div>
+                <div class="flex flex-col mx-auto text-center pt-5 gap-3">
+                    <h1 class="text-xl font-bold">Cek Sosial Media Kami</h1>
+                    <div class="flex justify-between gap-4 mx-auto">
+                        <div class="hover:scale-125 transition-transform duration-300">
+                            <a href="https://www.instagram.com/sport_is_number_one_2024?igsh=dmZnYjE5cXpiYjkw&utm_source=qr" target="_blank">
+                                <img src="{{URL::asset('images/instagram.png')}}" alt="" class="w-10">
+                            </a>
+                        </div>
+                        <div>
+                            <a href="http://www.tiktok.com/@sport.is.number.one" target="_blank">
+                                <img src="{{URL::asset('images/tik-tok.png')}}" alt="" class="w-10">
+                            </a>
+                        </div>
+                        <div>
+                            <a href="https://wa.me/+6283879822568" target="_blank">
+                                <img src="{{URL::asset('images/whatsapp.png')}}" alt="" class="w-10">
+                            </a>
+                        </div>
+                        <div>
+                            <a href="https://www.youtube.com/@dandielfanni1239" target="_blank">
+                                <img src="{{URL::asset('images/youtube.png')}}" alt="" class="w-10">
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </article>
     </section>
